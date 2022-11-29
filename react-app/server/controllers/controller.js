3cfdba9a606c8327eea41986e1fecbbdab34fd1a
AlexPrezeau <114449515+AlexPrezeau@users.noreply.github.com>
2022-11-28 19:32:32 -0500

    Updated SourceModel and controller

--- react-app/server/controllers/controller.js
+++ react-app/server/controllers/controller.js
@@ -1,4 +1,5 @@
 const data = require("../data");
+const sources= require('../models/SourceModel');
 
 module.exports = {
     hello: (req, res) => {
@@ -6,20 +7,40 @@ module.exports = {
     },
 
     getSources: (req, res) => {
-                  //key      //response             //an attribute of our response
+        //key      //response             //an attribute of our response
         res.json({"message": "We got your sources!", data: data.sources});
+        sources.find({}) //find all sources
+        .then(sourceData => {
+            console.log(sourceData); //print them all out
+            res.json({"message": "OK", data: sourceData});
+        }) // after you find the sources, re
+        .catch(err => { //try catch
+            console.log(err); //print the error
+            res.json(err);
+        });
     },
 
     deleteSource: (req, res) => {
-        //specifying the condition under which we filter
-        data.sources = data.sources.filter(sourceToDelete => sourceToDelete.id !== req.params.id);
-        res.json({"message": "Source deleted!", data: data.sources});
+       sources.findOneAndDelete({id:req.params.id})
+       .then(sourceData => {
+            console.log(sourceData);
+            res.json({"message": "OK", data: sourceData});
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
     },
 
     addSource: (req, res) => {
-        //refs our data //.push = adding to our data 
-        data.sources.push(req.body.source); // () = this is something we grab from user input
-        res.json({"message": "Source added!", data: data.sources});
+        sources.create(req.body.source)
+        .then(sourceData => {
+            console.log(sourceData);
+            res.json({"message": "OK", data: sourceData});
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
     }
 };
-
